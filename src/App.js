b29b15054dc3622af804ae2bb70e06d35189e1a4
Muhammad Hashim <muhammadhashimsardar@gmail.com>
2023-11-29 22:10:53 +0500

    feat: :white_check_mark: Add task 5  Counter App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from "react";
 function App() {
+
+
+   const [count, setCount] = useState(0);
+
+   // Event handler to increment the count
+   const increment = () => {
+     setCount(count + 1);
+   };
+
+   // Event handler to decrement the count
+   const decrement = () => {
+     setCount(count - 1);
+   };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container mx-auto mt-48 text-center">
+      <h1 className="text-4xl font-bold mb-4">Counter App</h1>
+      <p className="text-2xl mb-4">Count: {count}</p>
+      <div className="flex justify-center ">
+        <button
+          className="bg-green-500 text-white px-4 py-2 mr-4 rounded"
+          onClick={increment}
         >
-          Learn React
-        </a>
-      </header>
+          Increment
+        </button>
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded"
+          onClick={decrement}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
